fd12755fc32f2cf1dd54ad59095ac34429a18f92
Raghav Sharma <code.practice17@gmail.com>
2025-03-19 18:54:31 +0530

    Added user input processing

--- main.c
+++ main.c
@@ -3,17 +3,26 @@
 #include <stdlib.h>
 
 
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+
 struct termios orig_termios;
 
 
 void enable_raw_mode(void);
 void disable_raw_mode(void);
+char read_input(void);
+void process_input(void);
 
 
 int
 main(void)
 {
     enable_raw_mode();
+
+    while(1) {
+        process_input();
+    }
 
     return 0;
 }
@@ -44,4 +53,26 @@ disable_raw_mode(void)
 {
     /* restore original terminal attributes */
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+
+void
+process_input(void)
+{
+    char ch = read_input();
+
+    if(ch == CTRL_KEY('q')) exit(0);
+
+}
+
+
+char
+read_input(void)
+{
+    char ch;
+
+    /* read 1 byte from standard input file descriptor */
+    read(STDIN_FILENO, &ch, 1);
+
+    return ch;
 }
