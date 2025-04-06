0da1d9a90bb82c351a4ed3596205de0dc7403413
Raghav Sharma <code.practice17@gmail.com>
2025-04-06 20:29:33 +0530

    Improved logic

--- main.c
+++ main.c
@@ -198,23 +198,27 @@ move_cursor(int ch)
 {
     switch(ch) {
         case ARROW_UP:
-            if(attributes.cy == 0) break;
-            attributes.cy--;
+            if(attributes.cy != 0) {
+                attributes.cy--;
+            }
             break;
 
         case ARROW_DOWN:
-            if(attributes.cy == attributes.rows - 1) break;
-            attributes.cy++;
+            if(attributes.cy != attributes.rows - 1) {
+                attributes.cy++;
+            }
             break;
 
         case ARROW_LEFT:
-            if(attributes.cx == 0) break;
-            attributes.cx--;
+            if(attributes.cx != 0) {
+                attributes.cx--;
+            }
             break;
 
         case ARROW_RIGHT:
-            if(attributes.cx == attributes.cols - 1) break;
-            attributes.cx++;
+            if(attributes.cx != attributes.cols - 1) {
+                attributes.cx++;
+            }
             break;
     }
 }
