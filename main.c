651bcb084ca19bd299011d1111e2e462bf2ac614
Raghav Sharma <code.practice17@gmail.com>
2025-04-23 17:46:59 +0530

    Added custom file argument

--- main.c
+++ main.c
@@ -58,11 +58,13 @@ void die(const char *s);
 
 
 int
-main(void)
+main(int argc, char *argv[])
 {
     enable_raw_mode();
     init_editor();
-    editor_open("test.txt");
+    if(argc == 2) {
+        editor_open(argv[1]);
+    }
 
     while(1) {
         refresh_screen();
